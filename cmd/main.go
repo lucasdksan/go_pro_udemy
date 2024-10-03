7e903156a067be39b54e26e67ec320f68a3f2859
lucasdksan <lucas.leoncio.silva@gmail.com>
2024-10-02 22:55:35 -0300

    Alterando o header da Request

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "go_pro/examples"
+import (
+	"fmt"
+	"net/http"
+)
+
+func noteList(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Listagem de Notas")
+}
+
+func noteView(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Visualizar uma Nota")
+}
+
+func noteCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprint(w, "Criar uma Nota")
+}
 
 func main() {
-	examples.Exec_6()
+	fmt.Println("Servidor rodando na porta 5000")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/notes", noteList)
+	mux.HandleFunc("/notes/view", noteView)
+	mux.HandleFunc("/notes/create", noteCreate)
+
+	if err := http.ListenAndServe(":5050", mux); err != nil {
+		panic("Server Error!")
+	}
 }
